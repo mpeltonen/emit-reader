fbd519d04216f83febbe0b552f004b5327b553fc
Mikko Peltonen <mikko.peltonen@reaktor.fi>
2012-11-10 21:04:11 +0200

    Perform checksum check inside for comprehension

--- src/main/scala/emitreader/Decoder.scala
+++ src/main/scala/emitreader/Decoder.scala
@@ -5,7 +5,7 @@ import akka.util.ByteString
 import java.nio.ByteOrder
 
 object Decoder {
-  def full: IO.Iteratee[(Long, Int, Seq[(Int, Int)])] = try {
+  def full: IO.Iteratee[(Long, Int, Seq[(Int, Int)])] =
     for {
       prefix             <- prefixDecoder
       id                 <- IO.take(3)
@@ -17,21 +17,17 @@ object Decoder {
       punchingData       <- IO.take(150)
       unused3            <- IO.take(56)
       checksum2          <- IO.take(1)
+      _                  <- if (checksumOk(prefix ++ id ++ unused1 ++ cardProductionWeek ++ cardProductionYear ++ unused2 ++ checksum1 ++ punchingData ++ unused3 ++ checksum2))
+                              IO.Done()
+                            else
+                              IO.Failure(new IllegalStateException("Checksum error"))
     } yield {
-      val allBytes = prefix ++ id ++ unused1 ++ cardProductionWeek ++ cardProductionYear ++ unused2 ++ checksum1 ++ punchingData ++ unused3 ++ checksum2
-      if (checksumOk(allBytes)) {
-        val iterator = punchingData.iterator
-        val punches = for (i <- 0 until 50) yield (unsigned(iterator.getByte), iterator.getShort(ByteOrder.LITTLE_ENDIAN).toInt)
-        (System.currentTimeMillis, decodeId(id), punches)
-      } else {
-        throw new IllegalStateException("Checksum error")
-      }
+      val iterator = punchingData.iterator
+      val punches = for (i <- 0 until 50) yield (unsigned(iterator.getByte), iterator.getShort(ByteOrder.LITTLE_ENDIAN).toInt)
+      (System.currentTimeMillis, decodeId(id), punches)
     }
-  } catch {
-    case e: Exception => IO.Failure(e)
-  }
 
-  def timeOnly: IO.Iteratee[(Long, Int, Seq[(Int, Int)])] = try {
+  def timeOnly: IO.Iteratee[(Long, Int, Seq[(Int, Int)])] =
     for {
       prefix             <- prefixDecoder
       id                 <- IO.take(3)
@@ -40,14 +36,13 @@ object Decoder {
       cardProductionYear <- IO.take(1)
       unused2            <- IO.take(1)
       checksum1          <- IO.take(1)
+      _                  <- if (checksumOk(id ++ unused1 ++ cardProductionWeek ++ cardProductionYear ++ unused2 ++ checksum1))
+                              IO.Done()
+                            else
+                              IO.Failure(new IllegalStateException("Checksum error"))
     } yield {
-      val checksumBytes = id ++ unused1 ++ cardProductionWeek ++ cardProductionYear ++ unused2 ++ checksum1
-      if (checksumOk(checksumBytes)) (System.currentTimeMillis, decodeId(id), Seq())
-      else throw new IllegalStateException("Checksum error")
+      (System.currentTimeMillis, decodeId(id), Seq())
     }
-  } catch {
-    case e: IllegalStateException => IO.Failure(e)
-  }
 
   private def prefixDecoder = IO.takeUntil(ByteString(0xFF.toByte, 0xFF.toByte), true)
   private def checksumOk(bs: ByteString) = bs.nonEmpty && bs.sum % 256 == 0
